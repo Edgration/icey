fa43ce3b9f9b43d9d4f05d14dad78bd904ecdced
xianglujia <770279416@qq.com>
2018-08-23 18:00:30 +0800

    add local check

--- bzoj-checker.cpp
+++ bzoj-checker.cpp
@@ -67,7 +67,6 @@ int work(int num, string num_string, string dir) {
 	/* download data and cd into it*/
 	system(dow.c_str());  
 	
-	/* to do : if there is a floder named num don't unzip*/
 	system("ls > list2.txt");
 	FILE *fa = freopen("list2.txt", "r", stdin);
 	
@@ -177,6 +176,109 @@ int work(int num, string num_string, string dir) {
 	/* end data */
 }
 
+int work2(int num, string num_string, string dir) {
+	
+	string dow = (string)"wget " + (string)" -d " + num_string + string(" ") + num_string;
+	string exedir = num_string + (string)"/test";
+	string com = (string)"g++ " + dir + (string)" -o " + exedir + (string)" -w -O2 -lm -DONLINE_JUDGE";
+	
+	/* download data and cd into it*/
+	
+	system("ls > list2.txt");
+	FILE *fa = freopen("list2.txt", "r", stdin);
+	
+	bool al = 0, al2 = 0;
+	
+	system(com.c_str());
+	bool CeFlag = 1;
+	/* get list */
+	string getlist = string("ls ") + num_string + string(" > list.txt");
+	system(getlist.c_str());
+	FILE *list = freopen("list.txt", "r", stdin);
+	int ct = 1; int eded = 0;
+	
+	/* compile */
+	vector<string>lli;
+	while (~fscanf(list, "%s", book[ct])) {
+		string temp = book[ct]; 
+		if (temp == "test") CeFlag = 0, ct--;
+		else eded = ct; lli.push_back(temp);
+		ct++;
+	}
+	ct--; system("rm list.txt");
+	sort(lli.begin(), lli.end(), cmps);
+	/* test */
+	map<string, bool>tested; tested.clear();
+	
+	printf("\n============================================================\n");
+	
+	for (int cr = 1; cr <= ct; ++cr) {
+		string data_num = lli[cr - 1];
+		if (data_num == "list.txt" || data_num == "test") continue;
+		string Fr = (string)"echo " + data_num + string(" | cut -d. -f1") + (string)" > Fr";
+		string Su   = (string)"echo " + data_num + string(" | cut -d. -f2") + (string)" > Su"; 	
+		system(Fr.c_str()); system(Su.c_str());
+		FILE *sss = freopen("Fr", "r", stdin); 
+		fscanf(sss, "%s", ttemp); string Front = ttemp;
+		system("rm Fr");
+		FILE *ccc = freopen("Su", "r", stdin); 
+		fscanf(ccc, "%s", ttemp2); string Suf   = ttemp2;
+		system("rm Su");
+		if (tested[Front]) continue;
+		else tested[Front] = 1;
+		
+		printf("|%8s    ", Front.c_str());
+		fflush(stdout);
+			
+		if (CeFlag == 1) {
+			printf("|         \033[3;43m CompileError \033[0m         |");
+			fflush(stdout);
+			printf("     --     |\n"); fflush(stdout);
+			if (cr != ct - 1) printf("------------------------------------------------------------\n");
+			continue;
+		}
+			
+		string doit = (string)"{ time -p ./" + exedir + (string)" < " + num_string + string("/") + \
+		Front + string(".in >") + "checker; } 2>time_cnt";
+		
+		//cerr << doit << endl;
+		system(doit.c_str());
+		string chk  = "diff -b -q checker " + num_string + string("/") + Front + string(".out >ans");  
+		system(chk.c_str()); 
+		FILE *cur = freopen("ans", "r", stdin);
+		fscanf(cur, "%s", ttemp);
+		string te = ttemp;
+		bool Reflag = 0;
+		FILE *cur2 = freopen("time_cnt", "r", stdin);
+		scanf("%s", ttemp); string ReTest = ttemp;
+		if (ReTest != "real") Reflag = 1; scanf("%s", ttemp);
+		if (Reflag) {
+			printf("|         \033[3;45m RuntimeError \033[0m         |");
+			fflush(stdout);
+			printf("     --     |\n"); fflush(stdout);
+		}
+		else if (te != Front) { 
+			printf("|         \033[3;41m WrongAnswer! \033[0m         |");
+			fflush(stdout);
+			printf("%8ss   |\n", ttemp); fflush(stdout);
+		} //WA
+		else { 
+			printf("|         \033[3;42m RightOutPut! \033[0m         |");
+			fflush(stdout);
+			printf("%8ss   |\n", ttemp); fflush(stdout);
+		} //AC
+		system("rm ans"); system("rm checker"); system("rm time_cnt");
+		if (cr < eded - 1) printf("------------------------------------------------------------\n");
+	};
+	if (!CeFlag) {
+		string des = (string)"rm " + exedir;
+		system(des.c_str()); 
+	}
+	printf("============================================================\n\n");
+	
+	/* end data */
+}
+
 int main(int argc, char *argv[]) {
 	if (argc == 4) {
 		cout << endl;
@@ -184,14 +286,20 @@ int main(int argc, char *argv[]) {
 		if (pre == "-t" || pre == "--test") {
 			string num = argv[2]; string dir = argv[3]; 
 			work(READ(num), num, dir);
-		} else {
+		} 
+		else if (pre == "-l" || pre == "--local") {
+			string num = argv[2]; string dir = argv[3]; 
+			work(0, num, dir);
+		} 
+		else {
 			myexit(); return 0;
 		} 
 	}
 	else if (argc == 2) {
 		string temp = argv[1];
 		if (temp == "-h" || temp == "--help") {
-			printf("usage : [-t] [problem number] [cpp location]\n"); 
+			printf("usage : [-t] [problem number]   [cpp location]  download BZOJ-data and Test\n"); 
+			printf("        [-l] [data-floder address] [cpp location]  local-data checker\n");
 			printf("        [-h] help\n");
 			printf("        [-a] about\n"); 
 		} else if (temp == "-about" || temp == "-a") {
